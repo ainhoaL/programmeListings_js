8893d84c610188388c4fddeb72330facfe39c8db
nhoa <ainhoa.larumbe@gmail.com>
2015-11-29 11:24:22 +0000

    Show programmes in page
    Let's see the programmes in the page.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 
 import ListingService from './ListingService';
+import ListingView from './ListingView';
 
 function renderApp() {
     let div = document.getElementById('main');
 
     let service = new ListingService();
+    let view = new ListingView();
 
     //Let's check this is all working end to end
-    service.getProgrammesForLetterAndPage('x', 1).then((data) => {
-        div.textContent = data;
+    service.getProgrammesForLetterAndPage('z', 1).then((data) => {
+        view.displayListOfProgrammes(data.elements);
     }, (error) => {
         div.textContent = error.statusText;
     });
